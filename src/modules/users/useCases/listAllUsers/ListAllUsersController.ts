c3db98b16b113c047cdd644f491ee3cccc9e4dee
Kaic Pierre <kaicpierresilva2002@gmail.com>
2022-05-08 21:35:22 -0300

    finishing challenge 01

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,20 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      if (!user_id) {
+        return response.status(400).json({ error: "Missing header" });
+      }
+      if (Array.isArray(user_id)) {
+        return response.status(400).json({ error: "Invalid Header" });
+      }
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
